c3dc666f10d69cefeee663a2b710d375c32435a0
Nepo <braulio.rojas@islas.org.mx>
2020-08-05 22:02:48 -0700

    Refactoriza funciones

--- coneval/R/gini_by_state.R
+++ coneval/R/gini_by_state.R
@@ -7,3 +7,15 @@ gini_by_state <- function(data, state){
         .$ingreso %>% 
         ineq::ineq(type="Gini")
 }
+
+summarize_income_by_decil <- function(data){
+    summary_by <- data %>% 
+        dplyr::group_by(decil) %>%
+        dplyr::summarize(promedio = mean(ingreso))
+}
+
+summarize_income_by_state <- function(data){
+    summary <- data %>% 
+        dplyr::group_by(entidad_federativa) %>%
+        dplyr::summarize(promedio = mean(ingreso))
+}
--- tests/testthat/test_read_gini_data.R
+++ tests/testthat/test_read_gini_data.R
@@ -6,28 +6,26 @@ test_that("Data has the expected shape", {
   expect_equal(ncol(data), 3)
 })
 
-resumen_by_decil <- data %>% 
-  dplyr::group_by(decil) %>%
-  dplyr::summarize(promedio = mean(ingreso))
+
 
 test_that("resumen_by_decil has the expected shape", {
+  resumen_by_decil <- coneval::summarize_income_by_decil(data)
   expect_true("tbl" %in% class(resumen_by_decil))
   expect_equal(nrow(resumen_by_decil), 10)
   expect_equal(ncol(resumen_by_decil), 2)
 })
 
-resumen_by_entidad <- data %>% 
-  dplyr::group_by(entidad_federativa) %>%
-  dplyr::summarize(promedio = mean(ingreso))
 
 test_that("resumen_by_entidad has the expected shape", {
+  resumen_by_entidad <- coneval::summarize_income_by_state(data)
   expect_true("tbl" %in% class(resumen_by_entidad))
   expect_equal(nrow(resumen_by_entidad), 33)
   expect_equal(ncol(resumen_by_entidad), 2)
 })
 
-gini_sonora <- coneval::gini_by_state(data, "SONORA")
 
 test_that("gini_sonora has the expected shape", {
-  expect_equal(gini_sonora, 0.463, tolerance=1e-3)
+  expected_gini <- 0.463
+  obtained_gini <- coneval::gini_by_state(data, "SONORA")
+  expect_equal(obtained_gini, expected_gini, tolerance=1e-3)
 })
