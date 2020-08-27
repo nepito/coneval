07f7deb165ba8b7b0c7e81b87f6942da03d8c31b
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-08-26 19:45:56 -0700

    Agrega los resultados al reporte

---
+++ scr/resumen_inegi.R
@@ -0,0 +1,25 @@
+library("coneval")
+library("tidyverse")
+library("ggplot2")
+
+
+data <- readr::read_csv("data/raw/tabla2_3_basicos_inegi.csv", col_types = "fdf")
+ingreso_estado <- coneval::summarize_income_by_state(data)
+ingreso_estado$promedio <- round(ingreso_estado$promedio)
+write_csv(ingreso_estado, "reports/ingreso_estado.csv")
+ingreso_decil <- coneval::summarize_income_by_decil(data)
+ingreso_decil$promedio <- round(ingreso_decil$promedio)
+write_csv(ingreso_decil, "reports/ingreso_decil.csv")
+
+marcas_eje_y <- pretty(c(min(0), max(data$ingreso)))
+ggplot(data, aes(x = decil, y = ingreso)) + 
+    geom_boxplot() +
+    geom_jitter(shape=16, position=position_jitter(0.2)) +
+    theme_classic() +
+    scale_y_continuous(
+        expand = c(0,0),
+        limits = range(marcas_eje_y),
+        breaks = marcas_eje_y,
+        labels = scales::dollar
+    )
+ggsave("reports/boxplot_deciles.png")
