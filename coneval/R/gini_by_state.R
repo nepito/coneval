c805a4d0798f4be61888a2dfe1dfb9673228e6b6
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-08-26 22:45:41 -0700

    Implementa la función

--- coneval/R/gini_by_state.R
+++ coneval/R/gini_by_state.R
@@ -10,11 +10,13 @@ gini_by_state <- function(data, state){
 
 
 calculate_gini_all_states <- function(data){
-    gini_all_state <- tibble::tibble("a" = rep( 0, len=33), "b" = rep( 0, len=33))
+    gini_all_state <- data %>%
+        dplyr::group_by(entidad_federativa) %>%
+        dplyr::summarize(gini = ineq::ineq(ingreso, type="Gini"))
 }
 
 summarize_income_by_decil <- function(data){
-    summary_by <- data %>% 
+    summary_by <- data %>%
         dplyr::group_by(decil) %>%
         dplyr::summarize(promedio = mean(ingreso))
 }
